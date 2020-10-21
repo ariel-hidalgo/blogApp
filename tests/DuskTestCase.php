b4e3c5d7519ee0418c74595c98d2af006702bab2
Ariel Hidalgo <arielhidalgo808@gmail.com>
2020-10-20 22:51:22 -0300

    testing config files for dusk on github actions

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -36,7 +36,7 @@ abstract class DuskTestCase extends BaseTestCase
         ]);
 
         return RemoteWebDriver::create(
-            env('SELENIUM_URL' , 'http://localhost:9515'), DesiredCapabilities::chrome()->setCapability(
+            'http://localhost:9515', DesiredCapabilities::chrome()->setCapability(
                 ChromeOptions::CAPABILITY, $options
             )
         );
