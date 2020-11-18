57efbb5c20c77232e0d098c2a541d9ed55e1659c
Ariel Hidalgo <arielhidalgo808@gmail.com>
2020-11-18 20:35:24 -0300

    Small Changes

--- resources/views/blogs/series.blade.php
+++ resources/views/blogs/series.blade.php
@@ -1,5 +1,5 @@
 <x-blogs>
 <div class="mt-6 text-center">
-    <h1 class="text-4xl font-sans font-semibold text-white">Categoría: Sries</h1>
+    <h1 class="text-4xl font-sans font-semibold text-white">Catgoría: Sries</h1>
 </div>
 </x-blogs>
