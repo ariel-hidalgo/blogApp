97fc9f0abae2f5b2f9e3de9fb733b3f205fee0cd
Ariel Hidalgo <arielhidalgo808@gmail.com>
2020-11-23 16:10:30 -0300

    Add 'managerCanStorePost' test

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Post;
+
+class PostTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testGuestCanNotCreatePost()
+    {
+        $response = $this->get(route('posts.create'));
+        $response->assertStatus(302);
+    }   
+
+    public function testUserCanNotCreatePost()
+    {
+        $user = User::factory()->create(['role' => 'user']);
+        $response = $this->actingAs($user)->get(
+            route('posts.create')
+        );
+        $response->assertStatus(403);
+    }
+
+    public function testManagerCanCreatePost()
+    {
+        $user = User::factory()->create(['role' => 'manager']);
+        $response = $this->actingAs($user)->get(
+            route('posts.create')
+        );
+        $response->assertStatus(200);
+    }
+
+    public function testManagerCanStorePost()
+    {
+        $manager = User::factory()->create(['role' => 'manager']);
+        $user = User::factory()->create(['role' => 'user']);
+        $post = Post::factory()->make(['user_id' => $user->id])->toArray();
+        $response = $this->actingAs($manager)->post(
+            route('posts.store') , $post
+        );
+        $response->assertRedirect();
+    } 
+}
