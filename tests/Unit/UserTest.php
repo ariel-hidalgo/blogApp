e7326b2b7592eef31f6fe188002090f71a1a7d37
Ariel Hidalgo <arielhidalgo808@gmail.com>
2020-11-23 12:21:40 -0300

    Create userHasPost and CategoryAssigned

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Unit;
 
 use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 use Tests\TestCase;
 
 class UserTest extends TestCase
@@ -25,5 +27,20 @@ class UserTest extends TestCase
         $userIsManager = $user->isManager();
         $this->assertFalse($userIsManager);
     }
+
+    public function testUserHasCategoryAssigned()
+    {
+        $user = User::factory()->create();
+        $category = Category::factory()->create(['user_id' => $user->id]);
+        $this->assertEquals($user->id , $category->user->id);
+    }
+
+    public function testUserHasPostAssigned()
+    {
+        $user = User::factory()->create();
+        $post = Post::factory()->create(['user_id' => $user->id]);
+        $this->assertEquals($user->id , $post->user->id);
+    }
+
 
 }
